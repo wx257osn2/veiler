e19082f9f2ce2b870bac12ed6b040544514e4eb9
I <wx257osn2@yahoo.co.jp>
2012-12-14 10:43:28 +0900

    Add : Samples

---
+++ sample/areion.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include"../ullr/areion.hpp"
+
+int main(){
+  auto t=veiler::ullr::areion<int>(3,2,1);
+  for(auto n:t){std::cout<<n<<std::endl;}
+}
+
---
+++ sample/lupegemusing.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+#include<veiler/pelops/lupegem/using.hpp>
+
+int main(){
+  VEILER_PELOPS_LUPEGEM_USING((std::cout)(std::endl))
+  cout<<"test"<<endl;
+}
