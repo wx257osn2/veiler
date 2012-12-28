34bb5f836922e22c9fdc685c02d704a1daf5849e
I <wx257osn2@yahoo.co.jp>
2012-12-28 19:46:14 +0900

    Fix : Veiler.Istasha - Derived member cast off 'const'

--- istasha/istasha.hpp
+++ istasha/istasha.hpp
@@ -8,11 +8,11 @@ template<typename basetype_name>\
 struct class_name{\
   class_name();\
 private:\
-  static const basetype_name derived_name;\
+  static basetype_name derived_name;\
   constexpr int _veiler_istasha_dummy_func_(){return 0;};\
   __VA_ARGS__\
 };\
-template<typename basetype_name>const basetype_name class_name<basetype_name>::derived_name;\
+template<typename basetype_name>basetype_name class_name<basetype_name>::derived_name;\
 template<typename basetype_name>class_name<basetype_name>::class_name()
 
 VEILER_ISTASHA_CREATE(istasha,T,_veiler_istasha_derived_,){static_cast<istasha<T>>(_veiler_istasha_derived_)._veiler_istasha_dummy_func_();}
