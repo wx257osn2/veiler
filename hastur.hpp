79ddfff3722707737880412f0ed683e3cbb82127
I <wx257osn2@yahoo.co.jp>
2013-04-03 03:45:33 +0900

    Add : Veiler.Hastur

---
+++ hastur.hpp
@@ -0,0 +1,74 @@
+/*
+////////////////////////////////////////////////////////////////////////////////////////
+/////////                                Hastur                                /////////
+/////////                              Has Member                              /////////
+/////////    Language : C++11                                                  /////////
+/////////    Compiler : GCC(g++) version 4.8.0 20120923 (experimental)         /////////
+/////////               Clang(clang++) version 3.3 (trunk)                     /////////
+/////////     Version : 1.0                                                    /////////
+/////////      Status : Untested                                               /////////
+/////////        List : VEILER_HASTUR_TAG_CREATE(name) (macro)                 /////////
+/////////               veiler::hastur<has> (template class)                   /////////
+/////////                      veiler::hastur<has>::type<T>  (template class)  /////////
+/////////                      veiler::hastur<has>::value<T> (template class)  /////////
+/////////                      veiler::hastur<has>::func<T>  (template class)  /////////
+/////////      Author : I                                                      /////////
+////////////////////////////////////////////////////////////////////////////////////////
+
+1. What's this library? -- このライブラリについて
+    このライブラリはあるクラスに特定の名称のメンバが存在しているかを
+    確認するためのメタ関数ライブラリです。
+
+2. Usage -- 使い方
+    このファイルをインクルードして使います。
+    実装詳細はhastur/hastur.hppを参照してください。
+    
+    
+    A. Reference -- リファレンス
+        a. VEILER_HASTUR_TAG_CREATE(name)
+            veiler::hasturのtemplate引数に渡すメタ関数を生成します。
+            これは
+             struct hoge{～}
+            の形に展開されるので、typedefで束縛してhasturに渡してください。
+            nameには調べたいメンバの名前を指定します。
+            ex.)  typedef VEILER_HASTUR_TAG_CREATE(hoge) has_hoge_mf;
+        b. veiler::hastur<has>
+            第一引数hasに渡したメタ関数で指定した名称のメンバを
+            持っているかどうかを表すメタ関数をメンバに持ちます。
+                1. veiler::hastur<has>::type<T>  -  型
+                    Tがhasで指定した名称の型を持っているかどうかを表します。
+                    持っていればstd::true_type、持っていなければstd::false_typeを
+                    継承します。
+                2. veiler::hastur<has>::value<T> -  変数
+                    Tがhasで指定した名称の変数を持っているかどうかを表します。
+                    持っていればstd::true_type、持っていなければstd::false_typeを
+                    継承します。
+                3. veiler::hastur<has>::func<T>  -  関数
+                    Tがhasで指定した名称の関数を持っているかどうかを表します。
+                    持っていればstd::true_type、持っていなければstd::false_typeを
+                    継承します。
+            ex.)  struct foo{typedef int hoge;};
+                  veiler::hastur<has_hoge_mf>::type::value<foo>(); // is true
+
+3. Development Environment -- 開発環境
+    Vim 7.3 + GCC(g++)version 4.8.0 20120923 (experimental)
+              Clang(clang++)version 3.2 (trunk)
+    Ideone.com
+
+4. License and Exemption from responsibility -- ライセンスと免責
+    当ライブラリはVeiler Source Licenseにて公開されます。
+    また、(c)(R)等を明記していませんが社名、製品名などは
+    各社の登録商標または商標です。
+
+5. 更新情報
+    2013/4/3    1.0 作成。公開。
+
+
+
+Copyright (C) 2013 I
+  Distributed under the Veiler Source License 1.0
+
+*/
+
+#include"./hastur/hastur.hpp"
+
---
+++ hastur/hastur.hpp
@@ -0,0 +1,35 @@
+#ifndef _VEILER_HASTUR_HPP_INCLUDED
+#define _VEILER_HASTUR_HPP_INCLUDED
+
+#include<utility>
+
+namespace veiler{
+
+#define VEILER_HASTUR_TAG_CREATE(name)\
+  struct{\
+    template<typename T, typename = typename T::name>\
+     static auto _type(T x,int) -> std::true_type;\
+    template<typename T>\
+     static auto _type(T x,long) -> std::false_type;\
+    template<typename T, decltype(&T::name) = &T::name>\
+     static auto _value(T x,int) -> std::true_type;\
+    template<typename T>\
+     static auto _value(T x,long) -> std::false_type;\
+    template<typename T>\
+     static auto _func(T x,int) -> decltype(x.name(), std::true_type());\
+    template<typename T>\
+     static auto _func(T x,long) -> std::false_type;\
+  }
+
+template<typename has> struct hastur{
+  template<typename T>struct type  : decltype(has::_type(std::declval<T>(),0)){};
+  template<typename T>struct value : decltype(has::_value(std::declval<T>(),0)){};
+  template<typename T>struct func  : decltype(has::_func(std::declval<T>(),0)){};
+};
+
+}//End : namespace veiler
+
+#endif //_VEILER_HASTUR_HPP_INCLUDED
+
+//Copyright (C) 2013 I
+//  Distributed under the Veiler Source License 1.0.
