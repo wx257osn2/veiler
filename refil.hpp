19ee474654bc11e0aeadebb8baa9a4733ff9b8df
I <wx257osn2@yahoo.co.jp>
2014-05-26 02:22:19 +0900

    Add : Veiler.Refil (Undocumented)

---
+++ refil.hpp
@@ -0,0 +1,11 @@
+/*
+
+Undocumented coming soon
+
+Copyright (C) 2014 I
+  Distributed under the Veiler Source License 1.0
+
+*/
+
+#include "./refil/refil.hpp"
+
---
+++ refil/refil.hpp
@@ -0,0 +1,39 @@
+#ifndef VEILER_REFIL_HPP_INCLUDED
+#define VEILER_REFIL_HPP_INCLUDED
+
+#include<utility>
+#include<veiler/ullr/aedra.hpp>
+#include<veiler/troy.hpp>
+#include<veiler/fool/invoke.hpp>
+
+namespace veiler{
+
+template<typename T>
+class refil:public func_troy<T>{
+  T* t;
+ public:
+  constexpr refil(T& t)noexcept:t(veiler::ullr::aedra(t)){}
+  refil(T&&) = delete;
+  constexpr refil(const refil& x)noexcept:t(x.t){}
+  refil& operator=(const refil& x)noexcept{t = x.t;return *this;}
+  constexpr T& get()const noexcept{return *t;}
+  constexpr operator T&()const noexcept{return *t;}
+  template<typename... Args>
+  constexpr auto operator()(Args&&... args)const
+    ->decltype((invoke(t,std::forward<Args>(args)...))){
+         return invoke(t,std::forward<Args>(args)...);
+  }
+};
+
+template<typename T>
+constexpr refil<T> ref(T& t)noexcept{return t;}
+
+template<typename T>
+constexpr refil<const T> cref(const T& t)noexcept{return t;}
+
+}//End : namespace veiler
+
+#endif//VEILER_REFIL_HPP_INCLUDED
+
+//Copyright (C) 2014 I
+//  Distributed under the Veiler Source License 1.0.
