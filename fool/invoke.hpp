299e13a0cadd68e4521890b2763628ea726d3438
I <wx257osn2@yahoo.co.jp>
2014-12-13 18:14:26 +0900

    Fix : Veiler.Fool - eliminate unnecessary parentheses at decltype

--- fool/invoke.hpp
+++ fool/invoke.hpp
@@ -12,31 +12,31 @@ namespace fool{
 template<typename F, typename T, typename... Args>
 constexpr auto invoke(F&& f, T&& t, Args&&... ts)
   noexcept(noexcept((veiler::forward<T>(t).*f)(veiler::forward<Args>(ts)...)))
-        ->decltype(((veiler::forward<T>(t).*f)(veiler::forward<Args>(ts)...))){
+         ->decltype((veiler::forward<T>(t).*f)(veiler::forward<Args>(ts)...)){
              return (veiler::forward<T>(t).*f)(veiler::forward<Args>(ts)...);
 }
 template<typename F, typename T, typename... Args>
 constexpr auto invoke(F&& f, T&& t, Args&&... ts)
   noexcept(noexcept(((*veiler::forward<T>(t)).*f)(veiler::forward<Args>(ts)...)))
-        ->decltype((((*veiler::forward<T>(t)).*f)(veiler::forward<Args>(ts)...))){
+         ->decltype(((*veiler::forward<T>(t)).*f)(veiler::forward<Args>(ts)...)){
              return ((*veiler::forward<T>(t)).*f)(veiler::forward<Args>(ts)...);
 }
 template<typename F, typename T>
 constexpr auto invoke(F&& f, T&& t)
   noexcept(noexcept(veiler::forward<T>(t).*f))
-        ->decltype((veiler::forward<T>(t).*f)){
+         ->decltype(veiler::forward<T>(t).*f){
              return veiler::forward<T>(t).*f;
 }
 template<typename F, typename T>
 constexpr auto invoke(F&& f, T&& t)
   noexcept(noexcept((*veiler::forward<T>(t)).*f))
-        ->decltype(((*veiler::forward<T>(t)).*f)){
+         ->decltype((*veiler::forward<T>(t)).*f){
              return (*veiler::forward<T>(t)).*f;
 }
 template<typename F, typename... Args>
 constexpr auto invoke(F&& f, Args&&... args)
   noexcept(noexcept(veiler::forward<F>(f)(veiler::forward<Args>(args)...)))
-        ->decltype((veiler::forward<F>(f)(veiler::forward<Args>(args)...))){
+         ->decltype(veiler::forward<F>(f)(veiler::forward<Args>(args)...)){
              return veiler::forward<F>(f)(veiler::forward<Args>(args)...);
 }
 
