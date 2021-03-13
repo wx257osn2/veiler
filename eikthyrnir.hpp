d827f01faba481f4fbb41515af2de8b3872d64c3
I <wx257osn2@yahoo.co.jp>
2021-03-14 01:50:11 +0900

    Mod : Veiler.Eikthyrnir - std::result_of_t -> std::invoke_result_t

--- eikthyrnir.hpp
+++ eikthyrnir.hpp
@@ -1,4 +1,4 @@
-//Copyright (C) 2012-2017 I
+//Copyright (C) 2012-2017, 2021 I
 //  Distributed under the Boost Software License, Version 1.0.
 //  (See accompanying file LICENSE.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
